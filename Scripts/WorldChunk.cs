c3c9249a49127dec61b0460198e7f4fc7e3d3ca5
Eddie Federmeyer <eddiefedermeyer@gmail.com>
2024-01-30 16:45:57 -0600

    Moved files around

---
+++ Scripts/WorldChunk.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+
+namespace SuperMarioRehashed.Scripts;
+
+public partial class WorldChunk : Node2D
+{
+    
+    public WorldChunk()
+    {
+        PackedScene chunkScene = GD.Load<PackedScene>("res://Chunks/Chunk1.tscn");
+        GetNode<Node2D>("WorldManager").AddChild(chunkScene.Instantiate());
+        
+    }
+}
