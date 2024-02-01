09868bd40d7e868c9663d2f69713786aa8ee7dfb
Eddie Federmeyer <eddiefedermeyer@gmail.com>
2024-01-31 20:44:55 -0600

    Looping World Test
    + Looping world based on a static singleton "World Manager"

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Godot;
 
 namespace SuperMarioRehashed.Scripts;
@@ -6,10 +8,70 @@ public partial class Player : CharacterBody2D
 {
     private float _speed = 300.0f;
     private float _jumpSpeed = -400.0f;
-
+    private WorldManager _worldManager;
+    private int _currentChunkIndex = 0;
+    private LinkedList<Node2D> _activeChunks = new LinkedList<Node2D>();
+    
     // Get the gravity from the project settings so you can sync with rigid body nodes.
     private float _gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
+    public Player()
+    {
+        // Constructor
+    }
+
+    // We need to grab the auto loaded world manager!
+    public override void _Ready()
+    {
+
+    }
+    
+    private void RenderChunk(int i)
+    {
+        
+    }
+    
+    public override void _Process(double delta)
+    {
+        GD.Print($"Character Pos: <X={this.Position.X}, Y={this.Position.Y}>");
+        GD.Print($"Chunk Index {_currentChunkIndex}");
+        // From the singleton world manager, grab the current active chunks (current and adjacent chunks)
+        WorldManager worldManager = WorldManager.GetWorldManager();
+        
+        float chunkSizeX = worldManager.GetChunkWidth();
+        GD.Print($"Chunk Size in pixels: {chunkSizeX}");
+        if (this.Position.X <= 0)
+        {
+            _currentChunkIndex = WorldManager.NumChunks - 1;
+            this.GlobalTranslate(new Vector2(chunkSizeX * (WorldManager.NumChunks), 0));
+        }
+        else if (this.Position.X >= chunkSizeX * (WorldManager.NumChunks))
+        {
+            _currentChunkIndex = 0;
+            this.GlobalTranslate(new Vector2(chunkSizeX * (WorldManager.NumChunks) * -1, 0));
+        }
+        else if (this.Position.X > (chunkSizeX * (_currentChunkIndex + 1)))
+        {
+            // We want to increment the chunk index IF the player has passed the edge of the chunk boundary
+            _currentChunkIndex = (_currentChunkIndex + 1) % WorldManager.NumChunks;
+        }
+        
+        
+        Node2D currentChunk = worldManager.GetChunk(_currentChunkIndex);
+        if (currentChunk == null)
+        {
+            return;
+        }
+        
+        if (!GetTree().Root.GetChildren().Contains(currentChunk))
+        {
+            GetTree().Root.AddChild(currentChunk);
+            currentChunk.GlobalTranslate(new Vector2(chunkSizeX * (_currentChunkIndex), 0));
+        }
+            
+        GD.Print($"Loaded Chunk #{_currentChunkIndex}");
+    }
+    
     public override void _PhysicsProcess(double delta)
     {
         Vector2 velocity = Velocity;
@@ -24,12 +86,12 @@ public partial class Player : CharacterBody2D
         }
 
         // Get the input direction.
-        var direction = Input.GetAxis("move_left", "move_right");
+        float direction = Input.GetAxis("move_left", "move_right");
         velocity.X = direction * _speed;
         this.Velocity = velocity;
 
         // Get child Image Node
-        var sprite2D = GetNode<Sprite2D>("Sprite2D");
+        Sprite2D sprite2D = GetNode<Sprite2D>("Sprite2D");
         if (velocity.X != 0)
         {
             sprite2D.FlipH = !(velocity.X > 0); // Flip if NOT moving right
--- Scripts/WorldManager.cs
+++ Scripts/WorldManager.cs
@@ -2,44 +2,64 @@
 using System;
 using System.Collections.Generic;
 using Godot;
+using Godot.Collections;
 
 namespace SuperMarioRehashed.Scripts;
 
 public partial class WorldManager : Node2D
 {
 
-    private const int NumChunks = 5;
-    private readonly LinkedList<WorldChunk> _chunks = new LinkedList<WorldChunk>();
+    private static WorldManager _worldManager = null;
     
-    public WorldManager()
+    public const int NumChunks = 3;
+    private static readonly Array<Node2D> Chunks = new Array<Node2D>();
+    // A chunk is build by a WorldManager (Node2D) > TileMap > TileSet > ...
+    
+    public override void _Ready()
     {
         for (int i = 0; i < NumChunks; i++)
         {
-            // WorldChunk newChunk = new WorldChunk();
             int chunkNum = Random.Shared.Next(1, 4);
             GD.Print($"Using chunk #{chunkNum}");
-            PackedScene chunkScene = GD.Load<PackedScene>($"res://Prefabs/Chunks/Chunk{chunkNum}.tscn");
-            Node2D chunk = (Node2D)chunkScene.Instantiate();
-            
-            TileMap chunkTileMap = chunk.GetNode<TileMap>("TileMap");
-            float chunkSize = chunkTileMap.GetQuadrantSize() * chunkTileMap.Transform.Scale.X * 24f;
-            
-            // chunk.Position += new Vector2(chunkSize * i, 0);
-            chunk.MoveLocalX(chunkSize * i);
-            
-            this.AddChild(chunk);
-            
-            
-            // _chunks.AddLast(Chunk);
-            // var tilemap = newChunk.GetNode<TileMap>(".");
-            // var xPos = tilemap.CellQuadrantSize * tilemap.Transform.Scale.X;
-            // newChunk.MoveLocalX(xPos);
-            // this.AddChild(newChunk);
-            // GD.Print(_chunks.Count, xPos);
+            Node2D chunk = (Node2D)GD.Load<PackedScene>($"res://Prefabs/Chunks/Chunk{chunkNum}.tscn").Instantiate();
+            Chunks.Add(chunk);
         }
-    } 
+        
+    }
 
+    // We want a singleton world manager for each game!
+    public static WorldManager GetWorldManager()
+    {
+        if (_worldManager == null)
+        {
+            _worldManager = new WorldManager();
+        }
 
+        return _worldManager;
+    }
 
+    public Node2D GetChunk(int i)
+    {
+        if (i >= NumChunks || i < 0 || _worldManager == null)
+        {
+            // GD.Print($"Error! i = {i}, WorldManager = {_worldManager}");
+            return null;
+        }
 
+        Node2D ret = Chunks[i];
+        // GD.Print($"Successfully grabbed chunk of index {i} with value {ret}");
+        return (ret ?? GetChunk(++i));
+    }
+
+    // public float GetChunkWidth(int i)
+    // {
+    //     TileMap chunk = Chunks[i].GetNode<TileMap>("TileMap");
+    //     return chunk.GetUsedRect().Size.X * chunk.TileSet.TileSize.X * 3;
+    // }
+    
+    public float GetChunkWidth()
+    {
+        TileMap chunk = Chunks[0].GetNode<TileMap>("TileMap");
+        return chunk.GetUsedRect().Size.X * chunk.TileSet.TileSize.X * 3;
+    }
 }
