1436add10ca4677624aab4ffc21a0f8322db8c71
Eddie Federmeyer <eddiefedermeyer@gmail.com>
2024-03-27 19:53:36 -0500

    Added Ice flower!
    + Added ice flower + powerup
    + Generalized powerup functions
    + Player can now generate any attack effect given the ObjectType enum (as a string)
    - removed action_grab

--- Scripts/GameObjects/IceFlowerObject.cs
+++ Scripts/GameObjects/IceFlowerObject.cs
@@ -3,21 +3,21 @@ using SuperMarioRehashed.Scripts.Scenes;
 
 namespace SuperMarioRehashed.Scripts.GameObjects;
 
-public partial class FireFlowerObject : MyObject
+public partial class IceFlowerObject : MyObject
 {
     public override void OnCollide(Node2D other)
     {
         if (other is not Player p) return;
         
         // p.AddItem(ObjectType.FireFlower);
-        p.GivePowerup(ObjectType.FireFlower);
+        p.GivePowerup(ObjectType.IceFlower);
         Rpc(nameof(DestroyObject));
         
         // Create timer for 7 seconds, remove after that time!
-        GetTree().CreateTimer(7).Timeout += () =>
+        GetTree().CreateTimer(10).Timeout += () =>
         {
-            // p.RemoveItemType(ObjectType.FireFlower);
-            p.RemovePowerUp(ObjectType.FireFlower);
+            // p.RemoveItemType(ObjectType.IceFlower);
+            p.RemovePowerUp(ObjectType.IceFlower);
         };
     }
 }
---
+++ Scripts/Scenes/Projectiles/Iceball.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+
+namespace SuperMarioRehashed.Scripts.Scenes.Projectiles;
+
+public partial class Iceball : MyProjectile
+{
+    public override void _Ready()
+    {
+        // Override any properties here!
+    }
+    
+}
