69633b127be4a7374b40ebcf953481839b099a00
mvassalla <mvassa4@uic.edu>
2024-03-28 14:47:27 -0500

    healthbar error
    Error

--- Scripts/GameObjects/MushroomObject.cs
+++ Scripts/GameObjects/MushroomObject.cs
@@ -1,15 +1,15 @@
-﻿using Godot;
+using Godot;
 using SuperMarioRehashed.Scripts.Scenes;
 
 namespace SuperMarioRehashed.Scripts.GameObjects;
 
 public partial class MushroomObject : MyObject
 {
-    public override void OnCollide(Node2D other)
-    {
-        if (other is not Player p) return;
-        
-        p.AddItem(ObjectType.Mushroom);
-        Rpc(nameof(DestroyObject));
-    }
+	public override void OnCollide(Node2D other)
+	{
+		if (other is not Player p) return;
+		
+		p.AddItem(ObjectType.Mushroom);
+		Rpc(nameof(DestroyObject));
+	}
 }
